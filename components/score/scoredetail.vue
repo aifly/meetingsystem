55425ec61120ee0cedb9a32dff859732c350afef
aifly <aifly_xc@163.com>
2018-10-19 23:05:09 +0800

    a

--- components/score/scoredetail.vue
+++ components/score/scoredetail.vue
@@ -13,12 +13,39 @@
 		</div>
 		<div class='wm-statisitcs'>
 			<div>
-				<span> <label for="" style="opacity:0">1</label>	参与评分人数：{{scoreObj.partakenum}}</span><span>详情>> </span>
+				<span> <label for="" style="opacity:0">1</label>	参与评分人数：{{scoreObj.partakenum}}</span><span style="margin-left:20px;color:#ce0000">详情>> </span>
 			</div>
 			<div>统计时间：{{scoreObj.date}}<label for="" style="opacity:0">1</label></div>
 		</div>
 		<div class='wm-score-table'>
-			
+			<ul>
+				<li>
+					<div class="wm-scoreitem-groupname">
+						<div>
+							所属分组
+						</div>
+					</div>
+					<div class="wm-scoreitem-list">
+						<div class='wm-scoreitem' >
+							<div class="wm-scoreitem-name">评估指标</div>
+							<div class="wm-scoreitem-avgscore">平均分</div>
+						</div>
+					</div>
+				</li>
+				<li v-for="(item,i) in scoreObj.scoreList" :key='i'>
+					<div class="wm-scoreitem-groupname">
+						<div>
+							{{item.groupname}}
+						</div>
+					</div>
+					<div class="wm-scoreitem-list">
+						<div class='wm-scoreitem' v-for="(scoreitem,j) in item.list" :key='j'>
+							<div class="wm-scoreitem-name" :class='{"noborder":item.list.length%2===0}'>{{scoreitem.scoreitemname}}</div>
+							<div class="wm-scoreitem-avgscore" :class='{"noborder":item.list.length%2===0}'>{{scoreitem.avgscore}}分</div>
+						</div>
+					</div>
+				</li>
+			</ul>
 		</div>
 	</section>
 </template>
